9adea01b0e6bd790c44cb45e7c3e2016e39c6803
jannejjj <61980297+jannejjj@users.noreply.github.com>
2021-11-26 23:25:52 +0200

    Database set up and snippet post route created

---
+++ server/routes/api.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+const mongoose = require("mongoose");
+const Snippet = require("../models/Snippet")
+
+router.post('/snippet/', function(req, res, next) {
+    // Checking if an identical snippet exists - could also not be checked but decided to not allow duplicates
+    Snippet.findOne({snippet: req.body.snippet}, (err, snippet) => {
+        if(err) return next(err);
+        if(!snippet) {
+            new Snippet({
+                snippet: req.body.snippet,
+                timestamp: req.body.timestamp,
+                upvotes: req.body.upvotes,
+                downvotes: req.body.downvotes
+            }).save((err) => {
+                if(err) return next(err);
+                return res.status(200).send("Saved snippet to database.")
+            });
+        } else {
+            return res.status(403).send("Duplicate snippets are not allowed!");
+        }
+    })
+});
+
+router.get
+
+module.exports = router;
