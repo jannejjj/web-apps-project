de1d5b9beef3697ac0718823b84dd8ec2f474c75
jannejjj <61980297+jannejjj@users.noreply.github.com>
2021-12-13 18:23:56 +0200

    Authentication for posting comments / snippets complete

--- server/auth/validateToken.js
+++ server/auth/validateToken.js
@@ -3,14 +3,15 @@ const jwt = require("jsonwebtoken");
 module.exports = function (req, res, next) {
   const authHeader = req.headers["authorization"];
   let token;
-  if (authHeader) {
-    token = authHeader;
+  if (authHeader == undefined) {
+    return res.status(401).json({ error: "You are not logged in!" });
   } else {
-    token = null;
+    token = authHeader;
   }
   console.log(token);
+  /*
   if (token === null)
-    return res.status(401).json({ error: "You are not logged in!" });
+    return res.status(401).json({ error: "You are not logged in!" }); */
 
   jwt.verify(token, process.env.SECRET, (err, result) => {
     if (err) {
